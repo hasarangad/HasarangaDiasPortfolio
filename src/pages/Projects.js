6fb9a66b00231070a5242a74ba8ea23f20ab33ad
Hasaranga Dias <142646474+hasarangad@users.noreply.github.com>
2025-01-04 09:42:54 +0530

    Update Projects.js

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -16,43 +16,45 @@ export default function Projects() {
       <div className="project-container">
         {/* Project 1 */}
         <div className="project-box">
-          <h3 className="project-title">Project 1</h3>
-          <p className="project-description">Description of Project 1 goes here. Include details like technologies used, goals, and outcomes.</p>
+          <h3 className="project-title">VIRTUAL CALCULATOR</h3>
+          <p className="project-description">virtual calculator that uses finger movements to operate.</p>
         </div>
 
         {/* Project 2 */}
         <div className="project-box">
-          <h3 className="project-title">Project 2</h3>
-          <p className="project-description">Description of Project 2 goes here. Include details like technologies used, goals, and outcomes.</p>
+          <h3 className="project-title">CAREERBRIDGE</h3>
+          <p className="project-description">Platform to find a job, post a job.</p>
         </div>
       </div>
 
       <div className="project-container">
         {/* Project 1 */}
         <div className="project-box">
-          <h3 className="project-title">Project 1</h3>
-          <p className="project-description">Description of Project 1 goes here. Include details like technologies used, goals, and outcomes.</p>
+          <h3 className="project-title">MY PORTFOLIO</h3>
+          <p className="project-description">Created a responsive and dynamic personal
+ portfolio website to showcase skills, projects, and
+ experience as a software engineer. </p>
         </div>
 
         {/* Project 2 */}
         <div className="project-box">
-          <h3 className="project-title">Project 2</h3>
-          <p className="project-description">Description of Project 2 goes here. Include details like technologies used, goals, and outcomes.</p>
+          <h3 className="project-title">SNAKE GAME</h3>
+          <p className="project-description">It is a remake of nokia snake game.</p>
         </div>
       </div>
 
       <div className="project-container">
         {/* Project 1 */}
         <div className="project-box">
-          <h3 className="project-title">Project 1</h3>
-          <p className="project-description">Description of Project 1 goes here. Include details like technologies used, goals, and outcomes.</p>
+          <h3 className="project-title"> CLASS PRO</h3>
+          <p className="project-description">Web Site for Parent Teacher collaboration.</p>
         </div>
 
         {/* Project 2 */}
-        <div className="project-box">
+        {/* <div className="project-box">
           <h3 className="project-title">Project 2</h3>
           <p className="project-description">Description of Project 2 goes here. Include details like technologies used, goals, and outcomes.</p>
-        </div>
+        </div> */}
       </div>
 
 
